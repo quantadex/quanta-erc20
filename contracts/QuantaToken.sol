d22168daf4239e078e338a62d1fa902cb7a18a88
Quoc <quocble@gmail.com>
2018-08-27 12:48:50 -0700

    clean up

--- contracts/QuantaToken.sol
+++ contracts/QuantaToken.sol
@@ -8,29 +8,12 @@ contract QuantaToken is PausableToken {
     uint8   public  constant decimals = 9;
     uint256 public INITIAL_SUPPLY = 400000000 * (10 ** uint256(decimals));
 
-    modifier validDestination( address to )
-    {
-        require(to != address(0x0));
-        require(to != address(this));
-        _;
-    }
-
     function QuantaToken() 
     {
         admin = owner;
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
         Transfer(address(0x0), msg.sender, INITIAL_SUPPLY);
-    }
-
-    function transfer(address _to, uint _value) validDestination(_to) returns (bool) 
-    {
-        return super.transfer(_to, _value);
-    }
-
-    function transferFrom(address _from, address _to, uint _value) validDestination(_to) returns (bool) 
-    {
-        return super.transferFrom(_from, _to, _value);
     }
 
     event AdminTransferred(address indexed previousAdmin, address indexed newAdmin);
